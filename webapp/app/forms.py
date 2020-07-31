6ff2dffe45f09b311e71f4f1de8c2631eda91038
Chris Natoli <chrisnatoli@gmail.com>
2020-07-31 15:36:35 -0400

    Change group input form to two text areas, one for the generators and the other for the relation, displayed like a group presentation

--- webapp/app/forms.py
+++ webapp/app/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import TextField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired
 
-class InputForm(FlaskForm):
-    group = StringField('Group', validators=[DataRequired()])
+class GroupInputForm(FlaskForm):
+    generators = TextAreaField('Generators', validators=[DataRequired()])
+    relation = TextAreaField('Relation')
     submit = SubmitField('Break down')
--- webapp/app/routes.py
+++ webapp/app/routes.py
@@ -1,13 +1,15 @@
 from flask import render_template
 from app import app
-from app.forms import InputForm
+from app.forms import GroupInputForm
 
 @app.route('/', methods=['GET','POST'])
 @app.route('/index', methods=['GET','POST'])
 def index():
-    form = InputForm()
+    form = GroupInputForm()
     if form.validate_on_submit():
-        return render_template('index.html', form=form, input=form.group.data)
+        return render_template('index.html', form=form,
+                               generators=form.generators.data,
+                               relation=form.relation.data)
     return render_template('index.html', form=form)
 
 @app.route('/about')
