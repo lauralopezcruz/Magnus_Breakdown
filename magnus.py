045404b7926db61c12771b75d82cf66531a76dbc
Chris Natoli <chrisnatoli@gmail.com>
2020-08-27 16:42:16 -0400

    Clean up magnus breakdown code

--- magnus.py
+++ magnus.py
@@ -32,6 +32,10 @@ class Syllable:
         new_exp = -self.exp
         return Syllable(self.ltr, self.sub, new_exp)
 
+    def pow(self, power):
+        new_exp = self.exp * power
+        return Syllable(self.ltr, self.sub, new_exp)
+
     # Makes a new syllable with an additional subscript. If this syllable
     # already has a subscript, then the old subscript is moved into the letter.
     def add_subscript(self, new_subscript):
@@ -140,11 +144,9 @@ def str_to_word(string):
             syl_str = syl_str + string[i]
         else:
             if syl_str != "":
-                print(syl_str)
                 syllables.append(str_to_syllable(syl_str))
             syl_str = string[i]
 
-    print(syl_str)
     syllables.append(str_to_syllable(syl_str))
 
     return syllables
@@ -192,6 +194,19 @@ class Group:
         gens_str = ", ".join([str(g) for g in self.gens])
         rels_str = ", ".join([word_to_str(r) for r in self.rels])
         return "<" + gens_str + " | "  + rels_str + ">"
+
+    def __eq__(self, other):
+        if not isinstance(other, Group):
+            return False
+
+        same_gens1 = all([(g in other.gens) for g in self.gens])
+        same_gens2 = all([(g in self.gens) for g in other.gens])
+
+        # This doesn't account for cyclic shifts.
+        same_rels1 = all([(r in other.rels) for r in self.rels])
+        same_rels2 = all([(r in self.rels) for r in other.rels])
+
+        return all([same_gens1, same_gens2, same_rels1, same_rels2])
 
 def str_to_group(string):
     string = (string.replace("<","").replace(">","")
@@ -257,29 +272,23 @@ def get_new_letters(used_letters, num_letters, preferred_letters=[]):
 def exponent_sum(generator, relation):
     return sum([syl.exp for syl in relation if syl.base() == generator.base() ])
 
-# Here we rewrite relation so that the generator with exponent sum=0
-# is not in the first position relation and generators are already adjusted
-def remove_exp_zero(zero_generator,relation):
-    syl = relation[0]
-    if zero_generator.ltr == syl.ltr and zero_generator.sub == syl.sub:
-        return reduce_word(relation[1:] + [syl.inverse()])
-    else:
-        return relation
-
-def relation_prime(zero_generator,relation):
+def relation_prime(gen_exp0, relation):
     new_relation = [relation[0].add_subscript(0)]
-    for i in range(1,len(relation)):
-        if relation[i].ltr != zero_generator.ltr:
-            subscript = -exponent_sum(zero_generator, relation[:i])
+    for i in range(1, len(relation)):
+        if relation[i].base() != gen_exp0.base(): # changed from ltr to base()
+            subscript = -exponent_sum(gen_exp0, relation[:i])
             syl = relation[i].add_subscript(subscript)
             new_relation.append(syl)
-    return(reduce_word(new_relation))
+    return reduce_word(new_relation)
 
-def smallest_subscript(letter,relation):
+def smallest_subscript(letter, relation):
     return min([syl.sub for syl in relation if syl.ltr == letter])
 
-def largest_subscript(letter,relation):
+def largest_subscript(letter, relation):
     return max([syl.sub for syl in relation if syl.ltr == letter])
+
+def word_length(word):
+    return sum([abs(syl.exp) for syl in word])
 
 
 
@@ -294,82 +303,94 @@ def magnus_case1(group, used_letters=set()):
 
     # Find a generator with exponent sum zero
     for generator in generators:
-        if exponent_sum(generator, relation)==0:
-            zero_generator = generator
+        if exponent_sum(generator, relation) == 0:
+            gen_exp0 = generator
             break
 
-    relation_shifted = remove_exp_zero(zero_generator, relation)
-    relation_p = relation_prime(zero_generator, relation_shifted)
+    # If the relation begins with gen_exp0, conjugate the relation
+    # to shift the first syllable to the end.
+    if gen_exp0.base() == relation[0].base():
+        relation = reduce_word(relation[1:] + [syl.inverse()])
 
+    relation_p = relation_prime(gen_exp0, relation)
+
+    ###########
+    #### I think this needs to be changed from ltr to base
     letters = {syl.ltr for syl in relation_p}
     new_generators = []
-    letter_max_sub={}
+    letter_max_sub = {}
     for letter in letters:
-        l_subscript = largest_subscript(letter,relation_p)
-        s_subscript = smallest_subscript(letter,relation_p)
-        letter_max_sub[letter]=l_subscript
+        l_subscript = largest_subscript(letter, relation_p)
+        s_subscript = smallest_subscript(letter, relation_p)
+        letter_max_sub[letter] = l_subscript
         for i in range(s_subscript, l_subscript+1):
             new_generators.append(Syllable(letter, i, 1))
+    new_group = Group(new_generators, [relation_p])
 
-    # Rewrite G as HNN extension
-    used_letters = used_letters.union({g.ltr[0] for g in new_generators}).union({g.ltr[0] for g in relation})
-    t=get_new_letters(used_letters,1,["t"])[0]
-    used_letters = used_letters.union(t)
-    new_generators_of_group=new_generators.copy()
-    new_generators_of_group.append(Syllable(t,"",1))
-    new_relations_of_group=[relation_p]
-    for generator in new_generators:
-        if generator.sub != letter_max_sub[generator.ltr]:
-            new_relations_of_group.append([Syllable(t,"",-1),
-                                           generator,
-                                           Syllable(t,"",1),
-                                           Syllable(generator.ltr,generator.sub+1,generator.exp*-1)])
+    # Fetch a new letter for the HNN extension's stable letter
+    used_letters = (used_letters
+                    .union({g.ltr[0] for g in new_generators})
+                    .union({g.ltr[0] for g in relation}))
+    stable_ltr = get_new_letters(used_letters, 1 , ["t"])[0]
+    used_letters = used_letters.union(stable_ltr)
 
-    return [Group(new_generators_of_group, new_relations_of_group),
-            Group(new_generators, [relation_p]),
-            used_letters]
+    # Rewrite original group as HNN extension of new_group
+    HNN_generators = new_generators.copy()
+    HNN_generators.append(Syllable(stable_ltr, "", 1))
+    HNN_relations = [relation_p]
+    for gen in new_generators:
+        if gen.sub != letter_max_sub[gen.ltr]:
+            conjugation = [Syllable(stable_ltr, "", -1),
+                           gen,
+                           Syllable(stable_ltr, "", 1),
+                           Syllable(gen.ltr, gen.sub+1, -1)]
+            HNN_relations.append(conjugation)
+    HNN = Group(HNN_generators, HNN_relations)
 
-# This function rewrites a group G as a free product of free groups and a one
-# relator group. It outputs a one relator group where all gen appear in relation
-# and the free generators
+    return [HNN, new_group, used_letters]
+
+# Take a one-relator group, extract any free generators (generators that
+# don't appear in the relation), and return those free generators and
+# the one-relator group without those free generators.
 def free_gen_rewrite(group):
     generators = group.gens
     relation = reduce_word(group.rels[0])
-    free_generators=[]
-    relation_letters=[(syl.ltr,syl.sub) for syl in relation]
+    free_generators = []
+    relation_letters = [(syl.ltr, syl.sub) for syl in relation]
 
     for generator in generators:
-        if (generator.ltr,generator.sub) not in relation_letters:
+        if (generator.ltr, generator.sub) not in relation_letters:
             free_generators.append(generator)
 
-    new_generators=[]
+    new_generators = []
     for gen in generators:
         if gen not in free_generators:
             new_generators.append(gen)
 
     return [free_generators, Group(new_generators,[relation])]
 
-# Returns a group that does have a zero generator than you can then input into
-# magnus_case1
+# Returns a new one-relator group that has a generator with exponential sum 0
+# and contains the original group. The new group can now be used for case 1.
 def magnus_case2(group, used_letters=set()):
     generators = group.gens
+    relation = group.rels[0]
     gen0 = generators[0]
     gen1 = generators[1]
-    relation=group.rels[0]
-    exp_sum0 = exponent_sum(gen0,relation)
-    exp_sum1 = exponent_sum(gen1,relation)
+    exp_sum0 = exponent_sum(gen0,relation) # alpha
+    exp_sum1 = exponent_sum(gen1,relation) # beta
 
     used_letters = used_letters.union({gen.ltr[0] for gen in generators})
-    x,y = get_new_letters(used_letters,2,["x","y"]) #gets two new letters for new generators
+    x,y = get_new_letters(used_letters,2,["x","y"])
     used_letters = used_letters.union({x,y})
-    syl_0=Syllable(x,"",1)
-    syl_1=Syllable(y,"",1)
-    replacement0 = [syl_1, Syllable(x, "", -exp_sum1)] #yx^{-\beta}
-    replacement1 = [Syllable(x, "", exp_sum0)]         #x^{\alpha},x (syl_1) will be zero_gen now
+
+    syl_0 = Syllable(x,"",1) # Will now have exp sum 0
+    syl_1 = Syllable(y,"",1)
+    replacement0 = [syl_1, Syllable(x, "", -exp_sum1)] # yx^{-\beta}
+    replacement1 = [Syllable(x, "", exp_sum0)]         # x^{\alpha}
 
     new_relation = []
     for i in range(len(relation)):
-        if relation[i].ltr==gen0.ltr and relation[i].sub==gen0.sub:
+        if relation[i].base() == gen0.base():
             exp = relation[i].exp
             if exp >= 0:
                 replacements = replacement0 * exp
@@ -377,37 +398,38 @@ def magnus_case2(group, used_letters=set()):
                 replacements = invert_word(replacement0) * (-exp)
             new_relation.extend(replacements)
 
-        elif relation[i].ltr == gen1.ltr and relation[i].sub==gen1.sub:
+        elif relation[i].base() == gen1.base():
             exp = relation[i].exp
             if exp >= 0:
                 replacements = replacement1 * exp
             else:
                 replacements = invert_word(replacement1) * (-exp)
             new_relation.extend(replacements)
+
         else:
             new_relation.append(relation[i])
 
-    new_generators=[syl_0,syl_1]
-    new_generators.extend(generators[2:])
+    new_generators = [syl_0, syl_1] + generators[2:]
+    new_group = Group(new_generators, [reduce_word(new_relation)])
 
-    return [Group(new_generators,[reduce_word(new_relation)]),
-            used_letters]
+    return [new_group, used_letters]
+
 
 def magnus_breakdown(group):
     print(group)
-    hierarchy_of_groups=[group]
+    hierarchy_of_groups = [group]
 
     used_letters = {gen.ltr[0] for gen in group.gens}
 
-    while relation_length(hierarchy_of_groups[-1].rels[0])>1:
-        free_generators, group =free_gen_rewrite(group)
+    while word_length(hierarchy_of_groups[-1].rels[0]) > 1:
+        free_generators, group = free_gen_rewrite(group)
         generators = group.gens
         relation = reduce_word(group.rels[0])
 
-        zero_gen=None
+        zero_gen = None
         for gen in generators:
-            if exponent_sum(gen,relation)==0:
-                zero_gen=gen
+            if exponent_sum(gen, relation) == 0:
+                zero_gen = gen
                 break
         if zero_gen != None:
             [original_group, group, used_letters] = magnus_case1(group, used_letters)
